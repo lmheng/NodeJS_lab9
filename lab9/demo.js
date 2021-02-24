ad4c1506be7aeb66bb6c8864fc9f97a931634685
lmheng <heng.l.meng@gmail.com>
2021-02-24 22:16:02 +0800

    Completed Lab9

---
+++ lab9/demo.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const username = "Alex";
+const roles = ["admin"];
+const jwt_key = "my-secret";
+const jwt_exp = "5000";
+
+const token = jwt.sign({ username, roles }, jwt_key, { expiresIn: jwt_exp });
+console.log(token);
+
+jwt.verify(token, jwt_key, (err, data) => {
+  if (err) {
+    console.log("Token verification failed: ", err.message);
+  } else {
+    console.log("User: ", data);
+  }
+});
